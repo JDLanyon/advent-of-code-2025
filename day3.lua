0da7fb4e826e12a71c5868955ad575da78bcaa75
JDLanyon <jlany9@hotmail.com>
2025-12-04 15:35:49 +1000

    day 3 part 2

--- day3.lua
+++ day3.lua
@@ -36,5 +36,46 @@ function part_1()
 end
 
 
-print(part_1())
--- part_2()
+function part_2(digits)
+    -- read input
+  local file = io.open('inputs/day3.txt', 'r')
+  io.input(file)
+
+  -- lol they called it joltage again
+  local total_joltage = 0
+
+  for line in io.lines() do
+    local n = #line
+    if n < digits then return 0 end -- catch when the line length is less than requested digits
+    
+    -- Use a table to build the largest number
+    local stack = {}
+    local to_remove = n - digits  -- How many digits we can remove
+    
+    for i = 1, n do
+        local digit = tonumber(line:sub(i, i))
+        
+        -- while digits can be removed from stack and current digit is larger
+        while #stack > 0 and to_remove > 0 and digit > stack[#stack] do
+            table.remove(stack)
+            to_remove = to_remove - 1
+        end
+        
+        table.insert(stack, digit)
+    end
+    
+    -- Convert stack to the largest voltage
+    local largest_joltage = 0
+    for i = 1, digits do
+        largest_joltage = largest_joltage * 10 + stack[i]
+    end
+    
+    print("largest voltage for row " .. n .. ": " .. largest_joltage)
+    total_joltage = total_joltage + largest_joltage
+  end
+  return total_joltage
+end
+
+
+-- print(part_1())
+print(part_2(12))
